3619e81377d4344a7b94648b8527fb92a06bf1e1
Keith Gonyon <keithgonyon@freedomforever.com>
2025-09-15 16:07:31 -0500

    Updated install script to not call redudant sudo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Building ramp..."
 go build -o ramp .
 
 echo "Installing to /usr/local/bin..."
-sudo cp ramp /usr/local/bin/
+cp ramp /usr/local/bin/
 
 echo "✅ ramp installed successfully!"
 echo "You can now run 'ramp --help' from anywhere"
